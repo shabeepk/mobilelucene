b49a87f8c2244effb6d0754f8e094a67a94b8309
Lukhnos Liu <lukhnos@lukhnos.org>
2015-08-19 19:18:52 -0700

    Use pre-defined SPI resource
    Loading JAR resources can be cumbersome on mobile. That Lucene uses
    many JARs also complicates the matter, since care needs to be taken
    when merging JARs / producing DEX files.
    
    j2objc has the ability to load resource on iOS, but again this
    requires downstream projects to include resource files, which would
    make the library hard to integrate.
    
    This change makes SPIClassIterator load from predefined resource
    list.

--- lucene/core/src/java/org/apache/lucene/util/SPIClassIterator.java
+++ lucene/core/src/java/org/apache/lucene/util/SPIClassIterator.java
@@ -26,8 +26,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.ServiceConfigurationError;
 
@@ -40,11 +43,163 @@ import java.util.ServiceConfigurationError;
  * @lucene.internal
  */
 public final class SPIClassIterator<S> implements Iterator<Class<? extends S>> {
-  private static final String META_INF_SERVICES = "META-INF/services/";
+  private static final Map<String, List<String>> hardCodedServices;
+  static {
+    hardCodedServices = new HashMap<String, List<String>>();
+    List<String> list;
+
+    /*
+
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.analysis.util.CharFilterFactory", list);
+    list.add("org.apache.lucene.analysis.charfilter.HTMLStripCharFilterFactory");
+    list.add("org.apache.lucene.analysis.charfilter.MappingCharFilterFactory");
+    list.add("org.apache.lucene.analysis.fa.PersianCharFilterFactory");
+    list.add("org.apache.lucene.analysis.pattern.PatternReplaceCharFilterFactory");
+
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.analysis.util.TokenFilterFactory", list);
+    list.add("org.apache.lucene.analysis.tr.ApostropheFilterFactory");
+    list.add("org.apache.lucene.analysis.ar.ArabicNormalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.ar.ArabicStemFilterFactory");
+    list.add("org.apache.lucene.analysis.bg.BulgarianStemFilterFactory");
+    list.add("org.apache.lucene.analysis.br.BrazilianStemFilterFactory");
+    list.add("org.apache.lucene.analysis.cjk.CJKBigramFilterFactory");
+    list.add("org.apache.lucene.analysis.cjk.CJKWidthFilterFactory");
+    list.add("org.apache.lucene.analysis.ckb.SoraniNormalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.ckb.SoraniStemFilterFactory");
+    list.add("org.apache.lucene.analysis.commongrams.CommonGramsFilterFactory");
+    list.add("org.apache.lucene.analysis.commongrams.CommonGramsQueryFilterFactory");
+    list.add("org.apache.lucene.analysis.compound.DictionaryCompoundWordTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.compound.HyphenationCompoundWordTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.core.LowerCaseFilterFactory");
+    list.add("org.apache.lucene.analysis.core.StopFilterFactory");
+    list.add("org.apache.lucene.analysis.core.TypeTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.core.UpperCaseFilterFactory");
+    list.add("org.apache.lucene.analysis.cz.CzechStemFilterFactory");
+    list.add("org.apache.lucene.analysis.de.GermanLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.de.GermanMinimalStemFilterFactory");
+    list.add("org.apache.lucene.analysis.de.GermanNormalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.de.GermanStemFilterFactory");
+    list.add("org.apache.lucene.analysis.el.GreekLowerCaseFilterFactory");
+    list.add("org.apache.lucene.analysis.el.GreekStemFilterFactory");
+    list.add("org.apache.lucene.analysis.en.EnglishMinimalStemFilterFactory");
+    list.add("org.apache.lucene.analysis.en.EnglishPossessiveFilterFactory");
+    list.add("org.apache.lucene.analysis.en.KStemFilterFactory");
+    list.add("org.apache.lucene.analysis.en.PorterStemFilterFactory");
+    list.add("org.apache.lucene.analysis.es.SpanishLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.fa.PersianNormalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.fi.FinnishLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.fr.FrenchLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.fr.FrenchMinimalStemFilterFactory");
+    list.add("org.apache.lucene.analysis.ga.IrishLowerCaseFilterFactory");
+    list.add("org.apache.lucene.analysis.gl.GalicianMinimalStemFilterFactory");
+    list.add("org.apache.lucene.analysis.gl.GalicianStemFilterFactory");
+    list.add("org.apache.lucene.analysis.hi.HindiNormalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.hi.HindiStemFilterFactory");
+    list.add("org.apache.lucene.analysis.hu.HungarianLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.hunspell.HunspellStemFilterFactory");
+    list.add("org.apache.lucene.analysis.id.IndonesianStemFilterFactory");
+    list.add("org.apache.lucene.analysis.in.IndicNormalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.it.ItalianLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.lv.LatvianStemFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.CapitalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.CodepointCountFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.HyphenatedWordsFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.KeepWordFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.KeywordMarkerFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.KeywordRepeatFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.LengthFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.LimitTokenCountFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.LimitTokenOffsetFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.LimitTokenPositionFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.RemoveDuplicatesTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.StemmerOverrideFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.TrimFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.TruncateTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.WordDelimiterFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.ScandinavianFoldingFilterFactory");
+    list.add("org.apache.lucene.analysis.miscellaneous.ScandinavianNormalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.ngram.EdgeNGramFilterFactory");
+    list.add("org.apache.lucene.analysis.ngram.NGramFilterFactory");
+    list.add("org.apache.lucene.analysis.no.NorwegianLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.no.NorwegianMinimalStemFilterFactory");
+    list.add("org.apache.lucene.analysis.pattern.PatternReplaceFilterFactory");
+    list.add("org.apache.lucene.analysis.pattern.PatternCaptureGroupFilterFactory");
+    list.add("org.apache.lucene.analysis.payloads.DelimitedPayloadTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.payloads.NumericPayloadTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.payloads.TokenOffsetPayloadTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.payloads.TypeAsPayloadTokenFilterFactory");
+    list.add("org.apache.lucene.analysis.pt.PortugueseLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.pt.PortugueseMinimalStemFilterFactory");
+    list.add("org.apache.lucene.analysis.pt.PortugueseStemFilterFactory");
+    list.add("org.apache.lucene.analysis.reverse.ReverseStringFilterFactory");
+    list.add("org.apache.lucene.analysis.ru.RussianLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.shingle.ShingleFilterFactory");
+    list.add("org.apache.lucene.analysis.snowball.SnowballPorterFilterFactory");
+    list.add("org.apache.lucene.analysis.sr.SerbianNormalizationFilterFactory");
+    list.add("org.apache.lucene.analysis.standard.ClassicFilterFactory");
+    list.add("org.apache.lucene.analysis.standard.StandardFilterFactory");
+    list.add("org.apache.lucene.analysis.sv.SwedishLightStemFilterFactory");
+    list.add("org.apache.lucene.analysis.synonym.SynonymFilterFactory");
+    list.add("org.apache.lucene.analysis.th.ThaiWordFilterFactory");
+    list.add("org.apache.lucene.analysis.tr.TurkishLowerCaseFilterFactory");
+    list.add("org.apache.lucene.analysis.util.ElisionFilterFactory");
+
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.analysis.util.TokenizerFactory", list);
+    list.add("org.apache.lucene.analysis.core.KeywordTokenizerFactory");
+    list.add("org.apache.lucene.analysis.core.LetterTokenizerFactory");
+    list.add("org.apache.lucene.analysis.core.LowerCaseTokenizerFactory");
+    list.add("org.apache.lucene.analysis.core.WhitespaceTokenizerFactory");
+    list.add("org.apache.lucene.analysis.ngram.EdgeNGramTokenizerFactory");
+    list.add("org.apache.lucene.analysis.ngram.NGramTokenizerFactory");
+    list.add("org.apache.lucene.analysis.path.PathHierarchyTokenizerFactory");
+    list.add("org.apache.lucene.analysis.pattern.PatternTokenizerFactory");
+    list.add("org.apache.lucene.analysis.standard.ClassicTokenizerFactory");
+    list.add("org.apache.lucene.analysis.standard.StandardTokenizerFactory");
+    list.add("org.apache.lucene.analysis.standard.UAX29URLEmailTokenizerFactory");
+    list.add("org.apache.lucene.analysis.th.ThaiTokenizerFactory");
+    list.add("org.apache.lucene.analysis.wikipedia.WikipediaTokenizerFactory");
+
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.codecs.Codec", list);
+    list.add("org.apache.lucene.codecs.simpletext.SimpleTextCodec");
+
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.codecs.DocValuesFormat", list);
+    list.add("org.apache.lucene.codecs.memory.MemoryDocValuesFormat");
+    list.add("org.apache.lucene.codecs.memory.DirectDocValuesFormat");
+    list.add("org.apache.lucene.codecs.simpletext.SimpleTextDocValuesFormat");
+
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.codecs.PostingsFormat", list);
+    list.add("org.apache.lucene.codecs.blocktreeords.BlockTreeOrdsPostingsFormat");
+    list.add("org.apache.lucene.codecs.bloom.BloomFilteringPostingsFormat");
+    list.add("org.apache.lucene.codecs.memory.DirectPostingsFormat");
+    list.add("org.apache.lucene.codecs.memory.FSTOrdPostingsFormat");
+    list.add("org.apache.lucene.codecs.memory.FSTPostingsFormat");
+    list.add("org.apache.lucene.codecs.memory.MemoryPostingsFormat");
+    list.add("org.apache.lucene.codecs.simpletext.SimpleTextPostingsFormat");
+    list.add("org.apache.lucene.codecs.autoprefix.AutoPrefixPostingsFormat");
+    */
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.codecs.Codec", list);
+    list.add("org.apache.lucene.codecs.lucene50.Lucene50Codec");
+
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.codecs.DocValuesFormat", list);
+    list.add("org.apache.lucene.codecs.lucene50.Lucene50DocValuesFormat");
+
+    list = new ArrayList<String>();
+    hardCodedServices.put("org.apache.lucene.codecs.PostingsFormat", list);
+    list.add("org.apache.lucene.codecs.lucene50.Lucene50PostingsFormat");
+  }
 
   private final Class<S> clazz;
   private final ClassLoader loader;
-  private final Enumeration<URL> profilesEnum;
+  private boolean loaded;
   private Iterator<String> linesIterator;
   
   public static <S> SPIClassIterator<S> get(Class<S> clazz) {
@@ -69,58 +224,25 @@ public final class SPIClassIterator<S> implements Iterator<Class<? extends S>> {
   
   private SPIClassIterator(Class<S> clazz, ClassLoader loader) {
     this.clazz = clazz;
-    try {
-      final String fullName = META_INF_SERVICES + clazz.getName();
-      this.profilesEnum = (loader == null) ? ClassLoader.getSystemResources(fullName) : loader.getResources(fullName);
-    } catch (IOException ioe) {
-      throw new ServiceConfigurationError("Error loading SPI profiles for type " + clazz.getName() + " from classpath", ioe);
+
+    List<String> classList = hardCodedServices.get(clazz.getName());
+    if (classList == null || classList.isEmpty()) {
+      throw new ServiceConfigurationError("Error loading SPI profiles for type " + clazz.getName() + " from hard-coded services");
     }
+
     this.loader = (loader == null) ? ClassLoader.getSystemClassLoader() : loader;
     this.linesIterator = Collections.<String>emptySet().iterator();
+    this.loaded = false;
   }
   
   private boolean loadNextProfile() {
-    ArrayList<String> lines = null;
-    while (profilesEnum.hasMoreElements()) {
-      if (lines != null) {
-        lines.clear();
-      } else {
-        lines = new ArrayList<>();
-      }
-      final URL url = profilesEnum.nextElement();
-      try {
-        final InputStream in = url.openStream();
-        boolean success = false;
-        try {
-          final BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-          String line;
-          while ((line = reader.readLine()) != null) {
-            final int pos = line.indexOf('#');
-            if (pos >= 0) {
-              line = line.substring(0, pos);
-            }
-            line = line.trim();
-            if (line.length() > 0) {
-              lines.add(line);
-            }
-          }
-          success = true;
-        } finally {
-          if (success) {
-            IOUtils.close(in);
-          } else {
-            IOUtils.closeWhileHandlingException(in);
-          }
-        }
-      } catch (IOException ioe) {
-        throw new ServiceConfigurationError("Error loading SPI class list from URL: " + url, ioe);
-      }
-      if (!lines.isEmpty()) {
-        this.linesIterator = lines.iterator();
-        return true;
-      }
+    if (loaded) {
+      return false;
     }
-    return false;
+
+    loaded = true;
+    linesIterator = hardCodedServices.get(this.clazz.getName()).iterator();
+    return true;
   }
   
   @Override
@@ -140,8 +262,7 @@ public final class SPIClassIterator<S> implements Iterator<Class<? extends S>> {
       // don't initialize the class (pass false as 2nd parameter):
       return Class.forName(c, false, loader).asSubclass(clazz);
     } catch (ClassNotFoundException cnfe) {
-      throw new ServiceConfigurationError(String.format(Locale.ROOT, "An SPI class of type %s with classname %s does not exist, "+
-        "please fix the file '%s%1$s' in your classpath.", clazz.getName(), c, META_INF_SERVICES));
+      throw new ServiceConfigurationError(String.format(Locale.ROOT, "An SPI class of type %s with classname %s does not exist.", clazz.getName(), c));
     }
   }
   
