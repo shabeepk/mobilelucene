8cef647247e73f30a51652f47a5e3f75ee31dc21
Lukhnos Liu <lukhnos@lukhnos.org>
2015-08-30 13:55:53 -0700

    Reformat portmobile and add license headers

--- lucene/core/src/java/org/lukhnos/portmobile/file/Files.java
+++ lucene/core/src/java/org/lukhnos/portmobile/file/Files.java
@@ -1,5 +1,19 @@
 package org.lukhnos.portmobile.file;
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.lukhnos.portmobile.channels.utils.FileChannelUtils;
 import org.lukhnos.portmobile.file.attribute.BasicFileAttributes;
 
@@ -17,170 +31,163 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Files {
-    public static Path createDirectories(Path dir) throws IOException {
-        if (exists(dir)) {
-            if (isDirectory(dir)) {
-                return dir;
-            } else {
-                // Note: This does not throw FileAlreadyExistsException like Java 7's nio.
-                throw new IOException("Path is not a directory: " + dir);
-            }
-        }
-
-        createDirectories(dir.getParent());
-        if (dir.toFile().mkdir()) {
-            return dir;
-        }
-
-        throw new IOException("Failed creating directory: " + dir);
+  public static Path createDirectories(Path dir) throws IOException {
+    if (exists(dir)) {
+      if (isDirectory(dir)) {
+        return dir;
+      } else {
+        // Note: This does not throw FileAlreadyExistsException like Java 7's nio.
+        throw new IOException("Path is not a directory: " + dir);
+      }
     }
 
-    public static SeekableByteChannel newByteChannel(Path path, StandardOpenOption mustBeReadOnly) throws IOException
-    {
-        return FileChannelUtils.open(path, mustBeReadOnly);
+    createDirectories(dir.getParent());
+    if (dir.toFile().mkdir()) {
+      return dir;
     }
 
-    public static Path createFile(Path path) throws IOException {
-        if (!path.toFile().createNewFile()) {
-            throw new IOException("File cannot be created: " + path);
-        }
-        return path;
+    throw new IOException("Failed creating directory: " + dir);
+  }
+
+  public static SeekableByteChannel newByteChannel(Path path, StandardOpenOption mustBeReadOnly) throws IOException {
+    return FileChannelUtils.open(path, mustBeReadOnly);
+  }
+
+  public static Path createFile(Path path) throws IOException {
+    if (!path.toFile().createNewFile()) {
+      throw new IOException("File cannot be created: " + path);
+    }
+    return path;
+  }
+
+  public static long size(Path path) throws IOException {
+    return path.toFile().length();
+  }
+
+  public static boolean exists(Path path) {
+    return path.file.exists();
+  }
+
+  public static boolean notExists(Path path) {
+    return !exists(path);
+  }
+
+  public static boolean isDirectory(Path path) {
+    return path.file.isDirectory();
+  }
+
+  public static boolean isWritable(Path path) {
+    return path.file.canWrite();
+  }
+
+  public static FileOutputStream newOutputStream(Path path) throws IOException {
+    return new FileOutputStream(path.toFile());
+  }
+
+  public static FileInputStream newInputStream(Path path) throws IOException {
+    return new FileInputStream(path.toFile());
+  }
+
+  public static BufferedReader newBufferedReader(Path path, Charset charset) throws IOException {
+    return new BufferedReader(new InputStreamReader(new FileInputStream(path.toFile()), charset));
+  }
+
+  public static DirectoryStream<Path> newDirectoryStream(Path dir) throws IOException {
+    if (!Files.isDirectory(dir)) {
+      throw new IOException("Not a directory: " + dir);
     }
 
-    public static long size(Path path) throws IOException {
-        return path.toFile().length();
+    List<Path> paths = new ArrayList<Path>();
+    File[] files = dir.toFile().listFiles();
+    for (File file : files) {
+      paths.add(new Path(file));
+    }
+    return new DirectoryStream.SimpleDirectoryStream<Path>(paths);
+  }
+
+  public static boolean deleteIfExists(Path path) throws IOException {
+    if (exists(path)) {
+      if (!path.toFile().delete()) {
+        throw new IOException("Could not delete path: " + path);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static Path createTempFile(Path dir, String prefix, String suffix) throws IOException {
+    return new Path(File.createTempFile(prefix, suffix, dir.toFile()));
+  }
+
+  public static Path copy(Path source, Path target, StandardCopyOption... ignored) throws IOException {
+    if (isDirectory(source)) {
+      throw new UnsupportedOperationException("Directory copy not supported in this implementation");
     }
 
-    public static boolean exists(Path path) {
-        return path.file.exists();
+    FileChannel srcChannel = null;
+    FileChannel dstChannnel = null;
+    try {
+      srcChannel = newInputStream(source).getChannel();
+      dstChannnel = newOutputStream(target).getChannel();
+
+      long alreadyRead = 0;
+      long size = srcChannel.size();
+      while (alreadyRead < size) {
+        alreadyRead += dstChannnel.transferFrom(srcChannel, alreadyRead, size - alreadyRead);
+      }
+    } finally {
+      srcChannel.close();
+      dstChannnel.close();
     }
 
-    public static boolean notExists(Path path) {
-        return !exists(path);
+    return target;
+  }
+
+  public static void delete(Path path) throws IOException {
+    path.toFile().delete();
+  }
+
+  public static Path move(Path source, Path target, StandardCopyOption... ignored) throws IOException {
+    if (source.toFile().renameTo(target.toFile())) {
+      return target;
     }
 
-    public static boolean isDirectory(Path path) {
-        return path.file.isDirectory();
+    throw new IOException("Move from " + source + " to " + target + " failed");
+  }
+
+  public static Path walkFileTree(Path start, FileVisitor<? super Path> visitor) throws IOException {
+    if (Files.isDirectory(start)) {
+      visitor.preVisitDirectory(start, null);
+      for (File child : start.toFile().listFiles()) {
+        walkFileTree(new Path(child), visitor);
+      }
+      visitor.postVisitDirectory(start, null);
+    } else {
+      visitor.visitFile(start, null);
+    }
+    return start;
+  }
+
+  public static Path createTempDirectory(String prefix) throws IOException {
+    File tmpFile = File.createTempFile(prefix, "");
+    tmpFile.delete();
+    tmpFile.mkdir();
+    return new Path(tmpFile);
+  }
+
+  public static Path createTempDirectory(Path path, String prefix) throws IOException {
+    File tmpFile = File.createTempFile(prefix, "", path.toFile());
+    tmpFile.delete();
+    tmpFile.mkdir();
+    return new Path(tmpFile);
+  }
+
+  public static BasicFileAttributes readAttributes(Path path, Class<?> clz) throws NoSuchFileException {
+    if (exists(path)) {
+      return new BasicFileAttributes(path.toFile());
     }
 
-    public static boolean isWritable(Path path) {
-        return path.file.canWrite();
-    }
-
-    public static FileOutputStream newOutputStream(Path path) throws IOException {
-        return new FileOutputStream(path.toFile());
-    }
-
-    public static FileInputStream newInputStream(Path path) throws IOException {
-        return new FileInputStream(path.toFile());
-    }
-
-    public static BufferedReader newBufferedReader(Path path, Charset charset) throws IOException {
-        return new BufferedReader(new InputStreamReader(new FileInputStream(path.toFile()), charset));
-    }
-
-    public static DirectoryStream<Path> newDirectoryStream(Path dir) throws IOException {
-        if (!Files.isDirectory(dir)) {
-            throw new IOException("Not a directory: " + dir);
-        }
-
-        List<Path> paths = new ArrayList<Path>();
-        File[] files = dir.toFile().listFiles();
-        for (File file : files) {
-            paths.add(new Path(file));
-        }
-        return new DirectoryStream.SimpleDirectoryStream<Path>(paths);
-    }
-
-    public static boolean deleteIfExists(Path path) throws IOException {
-        if (exists(path)) {
-            if (!path.toFile().delete()) {
-                throw new IOException("Could not delete path: " + path);
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static Path createTempFile(Path dir, String prefix, String suffix) throws IOException {
-        return new Path(File.createTempFile(prefix, suffix, dir.toFile()));
-    }
-
-    public static Path copy(Path source, Path target, StandardCopyOption... ignored) throws IOException {
-        if (isDirectory(source)) {
-            throw new UnsupportedOperationException("Directory copy not supported in this implementation");
-        }
-
-        FileChannel srcChannel = null;
-        FileChannel dstChannnel = null;
-        try {
-            srcChannel = newInputStream(source).getChannel();
-            dstChannnel = newOutputStream(target).getChannel();
-
-            long alreadyRead = 0;
-            long size = srcChannel.size();
-            while (alreadyRead < size) {
-                alreadyRead += dstChannnel.transferFrom(srcChannel, alreadyRead, size - alreadyRead);
-            }
-        } finally {
-            srcChannel.close();
-            dstChannnel.close();
-        }
-
-        return target;
-    }
-
-    public static void delete(Path path) throws IOException {
-        path.toFile().delete();
-    }
-
-    public static Path move(Path source, Path target, StandardCopyOption... ignored) throws IOException {
-        if (source.toFile().renameTo(target.toFile())) {
-            return target;
-        }
-
-        throw new IOException("Move from " + source + " to " + target + " failed");
-    }
-
-    public static Path walkFileTree(Path start, FileVisitor<? super Path> visitor) throws IOException {
-        if (Files.isDirectory(start)) {
-            visitor.preVisitDirectory(start, null);
-            for (File child : start.toFile().listFiles()) {
-                walkFileTree(new Path(child), visitor);
-            }
-            visitor.postVisitDirectory(start, null);
-        } else {
-            visitor.visitFile(start, null);
-        }
-        return start;
-    }
-
-    public static Path createTempDirectory(String prefix) throws IOException {
-        File tmpFile = File.createTempFile(prefix, "");
-        tmpFile.delete();
-        tmpFile.mkdir();
-        return new Path(tmpFile);
-    }
-
-    public static Path createTempDirectory(Path path, String prefix) throws IOException {
-        File tmpFile = File.createTempFile(prefix, "", path.toFile());
-        tmpFile.delete();
-        tmpFile.mkdir();
-        return new Path(tmpFile);
-    }
-
-    public static BasicFileAttributes readAttributes(Path path, Class<?> clz) throws NoSuchFileException {
-        if (exists(path)) {
-            return new BasicFileAttributes(path.toFile());
-        }
-
-        throw new NoSuchFileException();
-    }
-
-    public static void main(String args[]) {
-        System.out.println("Empty path exists? " + Files.exists(Paths.get("")));
-        System.out.println("Absolute path exists? " + Files.exists(Paths.get("/usr")));
-        System.out.println("Contents: " + Arrays.asList(new File("/tmp").listFiles()));
-    }
+    throw new NoSuchFileException();
+  }
 }
--- lucene/core/src/java/org/lukhnos/portmobile/file/Path.java
+++ lucene/core/src/java/org/lukhnos/portmobile/file/Path.java
@@ -1,72 +1,72 @@
 package org.lukhnos.portmobile.file;
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.File;
 import java.io.IOException;
 
 public class Path {
-    File file;
+  File file;
 
-    Path(String path) {
-        file = new File(path);
+  Path(String path) {
+    file = new File(path);
+  }
+
+  Path(File file) {
+    this.file = file;
+  }
+
+  public File toFile() {
+    return file;
+  }
+
+  public Path toRealPath() throws IOException {
+    // We assume case sensitivity and other representations don't matter.
+    return this;
+  }
+
+  public Path getFileName() {
+    return new Path(file.getName());
+  }
+
+  public String toString() {
+    return file.toString();
+  }
+
+  public Path resolve(String other) {
+    if (other.isEmpty()) {
+      return this;
     }
 
-    Path(File file) {
-        this.file = file;
+    File otherFile = new File(other);
+    if (otherFile.isAbsolute()) {
+      return new Path(otherFile);
     }
 
-    public File toFile() {
-        return file;
-    }
+    return new Path(new File(this.file, other));
+  }
 
-    public Path toRealPath() throws IOException {
-        // We assume case sensitivity and other representations don't matter.
-        return this;
-    }
+  public boolean isAbsolute() {
+    return file.isAbsolute();
+  }
 
-    public Path getFileName() {
-        return new Path(file.getName());
-    }
+  public Path toAbsolutePath() {
+    return new Path(file.getAbsoluteFile());
+  }
 
-    public String toString() {
-        return file.toString();
-    }
-
-    public Path resolve(String other) {
-        if (other.isEmpty()) {
-            return this;
-        }
-
-        File otherFile = new File(other);
-        if (otherFile.isAbsolute()) {
-            return new Path(otherFile);
-        }
-
-        return new Path(new File(this.file, other));
-    }
-
-    public boolean isAbsolute() {
-        return file.isAbsolute();
-    }
-
-    public Path toAbsolutePath() {
-        return new Path(file.getAbsoluteFile());
-    }
-
-    public static void main(String args[]) {
-        // TODO: Implement and move tests.
-        System.out.println("oldcoffee");
-
-        Path p = Paths.get("/foo/bar/blah.txt");
-        System.out.println(p.toString());
-        System.out.println(p.getFileName().toString());
-
-        System.out.println(Paths.get("").toString());
-        System.out.println(Paths.get("/foo").resolve(""));
-        System.out.println(Paths.get("/foo").resolve("/bar"));
-        System.out.println(Paths.get("/foo").resolve("bar.txt"));
-    }
-
-    public Path getParent() {
-        return new Path(file.getParent());
-    }
+  public Path getParent() {
+    return new Path(file.getParent());
+  }
 }
